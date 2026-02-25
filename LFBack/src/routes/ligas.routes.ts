0a11ba16b45005d6136bb63b2f6abdc53147cc49
COMPUS023\daw2 <alejandrosainzsainz@outlook.es>
2026-02-25 16:47:41 +0100

    Eliminar imports sin usar en este archivo .routes

--- LFBack/src/routes/ligas.routes.ts
+++ LFBack/src/routes/ligas.routes.ts
@@ -7,10 +7,8 @@ import {
 } from "../controllers/ligas.controllers";
 import {validateStringParams} from "../middleware/validateStringParams.middleware";
 import {emptyFields} from "../middleware/emptyFields.middleware";
-import {existsUsuario} from "../middleware/legacy/userExistence.middleware";
 import {ligaExists} from "../middleware/ligaExists.middleware";
 import {ligaPlazasLibres} from "../middleware/ligaPlazasLibres.middleware";
-import {authMiddleware} from "../middleware/authmiddleware/auth.middleware";
 import {userTeamExistsInLiga} from "../middleware/userTeamExistsInLiga.middleware";
 
 
